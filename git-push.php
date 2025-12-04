8f83a68219fdc5ced14a2597dc9f6586e3165d70
prekerteam <prekerteam@gmail.com>
2025-12-04 19:08:46 +0700

    edit pasaran

--- git-push.php
+++ git-push.php
@@ -5,18 +5,18 @@ date_default_timezone_set('Asia/Jakarta');
 $commitMessage = "update kode - " . date('Y-m-d H:i:s');
 
 // CHANGE THIS: lokasi repo
-$repo = "/var/www/html/primbon";
+// $repo = "/var/www/html/primbon";
 
 // Langkah 1: git add
-$cmd1 = "cd $repo && git add . 2>&1";
+$cmd1 = "git add . 2>&1";
 exec($cmd1, $out1, $ret1);
 
 // Langkah 2: git commit
-$cmd2 = "cd $repo && git commit -m \"$commitMessage\" 2>&1";
+$cmd2 = "git commit -m \"$commitMessage\" 2>&1";
 exec($cmd2, $out2, $ret2);
 
 // Langkah 3: git push
-$cmd3 = "cd $repo && git push origin main 2>&1";
+$cmd3 = "git push origin main 2>&1";
 exec($cmd3, $out3, $ret3);
 
 // Tampilkan hasil
